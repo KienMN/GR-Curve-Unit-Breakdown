1eb552af14d215745cdddf7b24a14ae660b64d9a
Kien Mai Ngoc <kienmn97@gmail.com>
2019-04-03 15:46:57 +0700

    Init UnitBreaker class

---
+++ src/core/units.py
@@ -0,0 +1,18 @@
+class UnitBreaker():
+  def __init__(self, *args, **kwargs):
+    pass
+
+  def break_unit(self, *args, **kwargs):
+    pass
+
+  def stack_unit(self, *args, **kwargs):
+    pass
+
+  def detect_sharp_boundary(self, *args, **kwargs):
+    pass
+
+  def compute_lithofacies(self, *args, **kwargs):
+    pass
+
+  def label_shape_code(self, *args, **kwagrs):
+    pass
